31d3f438845165c86ce3b9e05921868bb5bc7c70
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-06-12 06:58:19 +0800

    Update package metadata

--- mix.exs
+++ mix.exs
@@ -7,26 +7,25 @@ defmodule Exslim.Mixfile do
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     source_url: "https://github.com/rstacruz/exslim",
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger]]
+    [applications: []]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  def package do
+    [
+      maintainers: ["Rico Sta. Cruz"],
+      licenses: ["MIT"],
+      files: ["lib", "mix.exs", "README.md"],
+      links: %{github: "https://github.com/rstacruz/exslim"}
+    ]
   end
 end
