9c8b66e0054f7f2e5475374a72b3d467c73a6f43
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-06-12 06:50:48 +0800

    Refactor tests

--- lib/exslim.ex
+++ lib/exslim.ex
@@ -1,4 +1,33 @@
 defmodule Exslim do
+  @doc """
+  Exslim.
+
+  ## AST
+
+    [ type: :document,
+      doctype: ...,
+      children: [
+        [ type: :element,
+          classes: [
+            [ type: :class, value: "blue" ],
+            [ type: :class, value: "small" ]
+          ],
+          id: [ type: :id, value: "box" ],
+          attributes: [
+            [ type: :attribute,
+              key: "href",
+              value: [
+                [ type: :expression, value: "\"hi\"" ]
+              ]
+            ]
+          ],
+          children: [
+            ...
+          ]
+        ]
+      ]
+    ]
+  """
   alias Exslim.Tokenizer
 
   def to_eex(str) do
