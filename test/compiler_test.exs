3e5cc8e6f32ac67062902c3fe4ef2ddf3efcc1bf
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-06-14 00:51:24 +0800

    Use pattern matching for compiler tests

--- test/compiler_test.exs
+++ test/compiler_test.exs
@@ -8,129 +8,169 @@ defmodule ExpugCompilerTest do
   test "doctype only" do
     {:ok, tokens} = tokenize("doctype html5")
     {:ok, ast} = compile(tokens)
-    assert ast ==
-      %{ doctype:
-        %{ token: {8, :doctype, "html5"},
-          type: :doctype,
-          value: "html5" },
-        type: :document }
+    assert %{
+      doctype: %{
+        token: {8, :doctype, "html5"},
+        type: :doctype,
+        value: "html5"
+      },
+      type: :document
+    } = ast
   end
 
   test "tag only" do
     {:ok, tokens} = tokenize("div")
     {:ok, ast} = compile(tokens)
-    assert ast ==
-      %{ type: :document,
-        children: [
-          %{name: "div", type: :element} ] }
+    assert %{
+      type: :document,
+      children: [%{
+        name: "div",
+        type: :element
+      }]
+    } = ast
   end
 
   test "doctype and tag" do
     {:ok, tokens} = tokenize("doctype html5\ndiv")
     {:ok, ast} = compile(tokens)
-    assert ast ==
-      %{ doctype:
-        %{ type: :doctype,
-          value: "html5",
-          token: {8, :doctype, "html5"} },
-        type: :document,
-        children:
-          [ %{name: "div", type: :element} ] }
+    assert %{
+      doctype: %{
+        type: :doctype,
+        value: "html5",
+        token: _
+      },
+      type: :document,
+      children: [%{
+        name: "div",
+        type: :element
+      }]
+    } = ast
   end
 
   test "doctype and tag and id" do
     {:ok, tokens} = tokenize("doctype html5\ndiv#box")
     {:ok, ast} = compile(tokens)
-    assert ast ==
-      %{ doctype:
-        %{ type: :doctype,
-          value: "html5",
-          token: {8, :doctype, "html5"}},
-        type: :document,
-        children:
-          [ %{id: "box",
-              name: "div",
-              type: :element} ] }
+    assert %{
+      doctype: %{
+        type: :doctype,
+        value: "html5",
+        token: _
+      },
+      type: :document,
+      children: [%{
+        id: "box",
+        name: "div",
+        type: :element
+      }]
+  } = ast
   end
 
   test "tag and classes" do
     {:ok, tokens} = tokenize("div.blue.small")
     {:ok, ast} = compile(tokens)
-    assert ast ==
-      %{ type: :document,
-      children: [
-        %{name: "div", type: :element, class: ["blue", "small"]}
-      ] }
+    assert %{
+      type: :document,
+      children: [%{
+        name: "div",
+        type: :element,
+        class: ["blue", "small"]
+      }]
+    } = ast
   end
 
   test "buffered text" do
     {:ok, tokens} = tokenize("div= hello")
     {:ok, ast} = compile(tokens)
-    assert ast ==
-      %{ type: :document,
-        children: [
-          %{ name: "div",
-            type: :element,
-            children: [
-              %{ type: :buffered_text, value: "hello" } ] } ] }
+    assert %{
+      type: :document,
+      children: [%{
+        name: "div",
+        type: :element,
+        children: [%{
+          type: :buffered_text,
+          value: "hello"
+        }]
+      }]
+    } = ast
   end
 
   test "doctype and tags" do
     {:ok, tokens} = tokenize("doctype html5\ndiv\nspan")
     {:ok, ast} = compile(tokens)
-    assert ast ==
-      %{ doctype:
-        %{ type: :doctype,
-          value: "html5",
-          token: {8, :doctype, "html5"} },
+    assert %{
+      doctype: %{
+        type: :doctype,
+        value: "html5",
+        token: {8, :doctype, "html5"}
+      },
       type: :document,
-      children:
-        [ %{name: "div", type: :element},
-          %{name: "span", type: :element} ] }
+      children: [%{
+        name: "div",
+        type: :element
+      }, %{
+        name: "span",
+        type: :element
+      }]
+    } = ast
   end
 
   test "nesting" do
     {:ok, tokens} = tokenize("head\n  title")
     {:ok, ast} = compile(tokens)
-    assert ast ==
-      %{ type: :document,
-        children:
-          [ %{ name: "head",
-              type: :element,
-              children:
-                [ %{ name: "title", type: :element } ] } ] }
+    assert %{
+      type: :document,
+      children: [%{
+        name: "head",
+        type: :element,
+        children: [%{
+          name: "title",
+          type: :element
+        }]
+      }]
+    } = ast
   end
 
   test "nesting deeper" do
     {:ok, tokens} = tokenize("head\n  title\n    span")
     {:ok, ast} = compile(tokens)
-    assert ast ==
-      %{ type: :document,
-        children:
-        [ %{ name: "head",
-            type: :element,
-            children:
-            [ %{ name: "title",
-                type: :element,
-                children:
-                [ %{ name: "span", type: :element } ] } ] } ] }
+    assert %{
+      type: :document,
+      children: [%{
+        name: "head",
+        type: :element,
+        children: [%{
+          name: "title",
+          type: :element,
+          children: [%{
+            name: "span",
+            type: :element
+          }]
+        }]
+      }]
+    } = ast
   end
 
   test "zigzag nesting" do
     {:ok, tokens} = tokenize("head\n  title\n    span\n  meta")
     {:ok, ast} = compile(tokens)
-    assert ast ==
-      %{ type: :document,
-        children:
-        [ %{ name: "head",
-            type: :element,
-            children:
-            [ %{ name: "title",
-                type: :element,
-                children:
-                [ %{name: "span", type: :element} ] },
-              %{ name: "meta",
-                type: :element } ] } ] }
+    assert %{
+      type: :document,
+      children: [%{
+        type: :element,
+        name: "head",
+        children: [%{
+          type: :element,
+          name: "title",
+          children: [%{
+            name: "span",
+            type: :element
+          }]
+        }, %{
+          name: "meta",
+          type: :element
+        }]
+      }]
+    } = ast
   end
 
   test "zigzag nesting error" do
